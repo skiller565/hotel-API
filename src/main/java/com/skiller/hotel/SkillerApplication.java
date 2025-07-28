16872106e2feafbf839caabb39f318c2239e6838
Emma Tola Laca <emmatola444@gmail.com>
2025-07-28 18:06:12 -0400

    Subiendo proyecto inicial

---
+++ src/main/java/com/skiller/hotel/SkillerApplication.java
@@ -0,0 +1,13 @@
+package com.skiller.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillerApplication.class, args);
+    }
+
+}
